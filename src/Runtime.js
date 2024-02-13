8ca37c0fdcbb50038c052fa433e385722cea056c
Inkflow <101215221+Inkflow59@users.noreply.github.com>
2024-02-13 11:34:36 +0100

    Patch du bug de saisie + changement de résolution

--- src/Runtime.js
+++ src/Runtime.js
@@ -1,25 +1,5 @@
 window['Runtime'] = (function Runtime(__can, __path){
 	var __scope = this;
-	// CService object
-	// -----------------------------------------------------------------
-	/* Copyright (c) 1996-2012 Clickteam
-	 *
-	 * This source code is part of the HTML5 exporter for Clickteam Multimedia Fusion 2.
-	 * 
-	 * Permission is hereby granted to any person obtaining a legal copy 
-	 * of Clickteam Multimedia Fusion 2 to use or modify this source code for 
-	 * debugging, optimizing, or customizing applications created with 
-	 * Clickteam Multimedia Fusion 2. 
-	 * Any other use of this source code is prohibited.
-	 *
-	 * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-	 * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-	 * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-	 * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-	 * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-	 * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
-	 * IN THE SOFTWARE.
-	 */
 
 	var CServices = {};
 
@@ -47226,7 +47206,7 @@ window['Runtime'] = (function Runtime(__can, __path){
 
 	    createList: function (file) {
 
-	        var extMaxHandle = 5;
+	        var extMaxHandle = 7;
 	        if (extMaxHandle) {
 	            this.extensions = new Array(extMaxHandle);
 	            this.numOfConditions = new Array(extMaxHandle);
@@ -47252,6 +47232,12 @@ window['Runtime'] = (function Runtime(__can, __path){
 	            this.addExt(e);
 	            e = new CExtLoad();
 	            e.handle = 4;
+	            this.addExt(e);
+	            e = new CExtLoad();
+	            e.handle = 5;
+	            this.addExt(e);
+	            e = new CExtLoad();
+	            e.handle = 6;
 	            this.addExt(e);
 	            // INCLUDE_ADDEXT
 	        }
@@ -47315,6 +47301,10 @@ window['Runtime'] = (function Runtime(__can, __path){
 	        return new CRunkcini();
 	        case 4:
 	        return new CRunHTML5();
+	        case 5:
+	        return new CRunKcCursor();
+	        case 6:
+	        return new CRunkcwctrl();
 	            // INCLUDE_NEWEXT
 	        }
 
@@ -55775,6 +55765,308 @@ window['Runtime'] = (function Runtime(__can, __path){
 		});
 
 
+
+	//----------------------------------------------------------------------------------
+	//
+	// CRUNKCCURSOR - Implementation of the Fusion Cursor object
+	//
+	//----------------------------------------------------------------------------------
+	/* Copyright (c) 1996-2012 Clickteam
+	 *
+	 * This source code is part of the HTML5 exporter for Clickteam Multimedia Fusion 2.
+	 *
+	 * Permission is hereby granted to any person obtaining a legal copy
+	 * of Clickteam Multimedia Fusion 2 to use or modify this source code for
+	 * debugging, optimizing, or customizing applications created with
+	 * Clickteam Multimedia Fusion 2.
+	 * Any other use of this source code is prohibited.
+	 *
+	 * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	 * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	 * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	 * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	 * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+	 * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+	 * IN THE SOFTWARE.
+	 */
+	CRunKcCursor.CND_LAST = 0;
+	CRunKcCursor.ACT_ACTION_SETSHAPE_APPSTARTING = 0;	// Standard arrow and small hourglass
+	CRunKcCursor.ACT_ACTION_SETSHAPE_ARROW = 1;	// Standard arrow
+	CRunKcCursor.ACT_ACTION_SETSHAPE_CROSS = 2;	// Crosshair
+	CRunKcCursor.ACT_ACTION_SETSHAPE_IBEAM = 3;	// Text I-beam
+	CRunKcCursor.ACT_ACTION_SETSHAPE_NO = 4;	// Slashed circle
+	CRunKcCursor.ACT_ACTION_SETSHAPE_SIZEALL = 5;	// Windows NT only: Four-pointed arrow
+	CRunKcCursor.ACT_ACTION_SETSHAPE_SIZENESW = 6;	// Double-pointed arrow pointing northeast and southwest
+	CRunKcCursor.ACT_ACTION_SETSHAPE_SIZENS = 7;	// Double-pointed arrow pointing north and south
+	CRunKcCursor.ACT_ACTION_SETSHAPE_SIZENWSE = 8;	// Double-pointed arrow pointing northwest and southeast
+	CRunKcCursor.ACT_ACTION_SETSHAPE_SIZEWE = 9;	// Double-pointed arrow pointing west and east
+	CRunKcCursor.ACT_ACTION_SETSHAPE_UPARROW = 10;	// Vertical arrow
+	CRunKcCursor.ACT_ACTION_SETSHAPE_WAIT = 11;	// Hourglass
+	CRunKcCursor.ACT_ACTION_SETSHAPE_HELP = 12;	// Hourglass
+	CRunKcCursor.ACT_ACTION_SETSHAPE_IMAGE_BYNUMBER = 13;	// Custom image (by number)
+	CRunKcCursor.ACT_ACTION_SETSHAPE_IMAGE_BYNAME = 14; // Custom image (by name)
+	CRunKcCursor.ACT_ACTION_SETSHAPE_HAND = 15;	// Hand
+	CRunKcCursor.ACT_ACTION_SETSHAPE_ZOOM = 16;	// Magnifying Glass
+	CRunKcCursor.ACT_ACTION_SETSHAPE_PICK = 17;	// Color Picker
+	CRunKcCursor.ACT_ACTION_SETSHAPE_FILL = 18;	// Fill
+	CRunKcCursor.ACT_ACTION_SETSHAPE_HSPLIT = 19;	// Horizontal Split
+	CRunKcCursor.ACT_ACTION_SETSHAPE_VSPLIT = 20;	// Vertical Split
+	CRunKcCursor.ACT_ACTION_SETSHAPE_DRAG_COPY = 21;	// Drag (Copy)
+	CRunKcCursor.ACT_ACTION_SETSHAPE_DRAG_MOVE = 22;	// Drag (Move)
+	CRunKcCursor.ACT_ACTION_SETSHAPE_DRAG_SHORTCUT = 23;	// Drag (Shortcut)
+	CRunKcCursor.ACT_ACTION_SETSHAPE_OBJECT = 24;	// Track active object
+	CRunKcCursor.EXP_LAST = 0;
+
+	function CRunKcCursor()
+	{
+		this.cursor = 'auto';
+	}
+
+	CRunKcCursor.prototype = CServices.extend(new CRunExtension(),
+		{
+			getNumberOfConditions: function ()
+			{
+				return CRunKcCursor.CND_LAST;
+			},
+
+			createRunObject: function (file, cob, version)
+			{
+				this.cursor = 'auto';
+				this.rh.rhApp.cursor = this.cursor;
+				return true;
+			},
+
+			action: function (num, act)
+			{
+				switch (num)
+				{
+					case CRunKcCursor.ACT_ACTION_SETSHAPE_APPSTARTING:
+						this.cursor = 'alias';
+						break;
+					case CRunKcCursor.ACT_ACTION_SETSHAPE_ARROW:
+						this.cursor = 'auto';
+						break;
+					case CRunKcCursor.ACT_ACTION_SETSHAPE_CROSS:
+						this.cursor = 'crosshair';
+						break;
+					case CRunKcCursor.ACT_ACTION_SETSHAPE_IBEAM:
+						this.cursor = 'text';
+						break;
+					case CRunKcCursor.ACT_ACTION_SETSHAPE_NO:
+						this.cursor = 'not-allowed';
+						break;
+					case CRunKcCursor.ACT_ACTION_SETSHAPE_SIZEALL:
+						this.cursor = 'all-scroll';
+						break;
+					case CRunKcCursor.ACT_ACTION_SETSHAPE_SIZENESW:
+						this.cursor = 'nesw-resize';
+						break;
+					case CRunKcCursor.ACT_ACTION_SETSHAPE_SIZENS:
+						this.cursor = 'ns-resize';
+						break;
+					case CRunKcCursor.ACT_ACTION_SETSHAPE_SIZENWSE:
+						this.cursor = 'nwse-resize';
+						break;
+					case CRunKcCursor.ACT_ACTION_SETSHAPE_SIZEWE:
+						this.cursor = 'ew-resize';
+						break;
+					case CRunKcCursor.ACT_ACTION_SETSHAPE_UPARROW:
+						this.cursor = 'pointer';
+						break;
+					case CRunKcCursor.ACT_ACTION_SETSHAPE_WAIT:
+						this.cursor = 'wait';
+						break;
+					case CRunKcCursor.ACT_ACTION_SETSHAPE_HELP:
+						this.cursor = 'help';
+						break;
+					case CRunKcCursor.ACT_ACTION_SETSHAPE_IMAGE_BYNUMBER:
+						break;
+					case CRunKcCursor.ACT_ACTION_SETSHAPE_IMAGE_BYNAME:
+						break;
+					case CRunKcCursor.ACT_ACTION_SETSHAPE_HAND:
+					    this.cursor = 'pointer';
+						break;
+					case CRunKcCursor.ACT_ACTION_SETSHAPE_ZOOM:
+						this.cursor = 'zoom-in';
+						break;
+					case CRunKcCursor.ACT_ACTION_SETSHAPE_PICK:
+						this.cursor = 'auto';
+						break;
+					case CRunKcCursor.ACT_ACTION_SETSHAPE_FILL:
+						this.cursor = 'auto';
+						break;
+					case CRunKcCursor.ACT_ACTION_SETSHAPE_HSPLIT:
+						this.cursor = 'col-resize';
+						break;
+					case CRunKcCursor.ACT_ACTION_SETSHAPE_VSPLIT:
+						this.cursor = 'row_resize';
+						break;
+					case CRunKcCursor.ACT_ACTION_SETSHAPE_DRAG_COPY:
+						this.cursor = 'copy';
+						break;
+					case CRunKcCursor.ACT_ACTION_SETSHAPE_DRAG_MOVE:
+						this.cursor = 'move';
+						break;
+					case CRunKcCursor.ACT_ACTION_SETSHAPE_DRAG_SHORTCUT:
+						this.cursor = 'context-menu';
+						break;
+					case CRunKcCursor.ACT_ACTION_SETSHAPE_OBJECT:
+						break;
+				}
+				this.rh.rhApp.cursor = this.cursor;
+				this.rh.showMouse();
+			}
+		});
+	//----------------------------------------------------------------------------------
+	//
+	// CRUNKCWCTRL Objet Window Control
+	//
+	//----------------------------------------------------------------------------------
+	/* Copyright (c) 1996-2012 Clickteam
+	 *
+	 * This source code is part of the HTML5 exporter for Clickteam Multimedia Fusion 2.
+	 *
+	 * Permission is hereby granted to any person obtaining a legal copy
+	 * of Clickteam Multimedia Fusion 2 to use or modify this source code for
+	 * debugging, optimizing, or customizing applications created with
+	 * Clickteam Multimedia Fusion 2.
+	 * Any other use of this source code is prohibited.
+	 *
+	 * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	 * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	 * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	 * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	 * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+	 * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+	 * IN THE SOFTWARE.
+	 */
+	CRunkcwctrl.CND_ISICONIC = 0;
+	CRunkcwctrl.CND_ISMAXIMIZED = 1;
+	CRunkcwctrl.CND_ISVISIBLE = 2;
+	CRunkcwctrl.CND_ISAPPACTIVE = 3;
+	CRunkcwctrl.CND_HASFOCUS = 4;
+	CRunkcwctrl.CND_ISATTACHEDTODESKTOP = 5;
+	CRunkcwctrl.CND_LAST = 6;
+	CRunkcwctrl.ACT_SETBACKCOLOR = 23;
+	CRunkcwctrl.EXP_GETXPOSITION = 0;
+	CRunkcwctrl.EXP_GETYPOSITION = 1;
+	CRunkcwctrl.EXP_GETXSIZE = 2;
+	CRunkcwctrl.EXP_GETYSIZE = 3;
+	CRunkcwctrl.EXP_GETSCREENXSIZE = 4;
+	CRunkcwctrl.EXP_GETSCREENYSIZE = 5;
+	CRunkcwctrl.EXP_GETSCREENDEPTH = 6;
+	CRunkcwctrl.EXP_GETCLIENTXSIZE = 7;
+	CRunkcwctrl.EXP_GETCLIENTYSIZE = 8;
+	CRunkcwctrl.EXP_GETTITLE = 9;
+	CRunkcwctrl.EXP_GETBACKCOLOR = 10;
+	CRunkcwctrl.EXP_GETXFRAME = 11;
+	CRunkcwctrl.EXP_GETYFRAME = 12;
+	CRunkcwctrl.EXP_GETWFRAME = 13;
+	CRunkcwctrl.EXP_GETHFRAME = 14;
+
+	function CRunkcwctrl()
+	{
+	}
+	CRunkcwctrl.prototype = CServices.extend(new CRunExtension(),
+		{
+			getNumberOfConditions: function ()
+			{
+				return CRunkcwctrl.CND_LAST;
+			},
+
+			// Conditions
+			// --------------------------------------------------
+			condition:             function (num, cnd)
+			{
+				switch (num)
+				{
+					case CRunkcwctrl.CND_ISICONIC:
+						return false;
+					case CRunkcwctrl.CND_ISMAXIMIZED:
+						return false;
+					case CRunkcwctrl.CND_ISVISIBLE:
+						return true;
+					case CRunkcwctrl.CND_ISAPPACTIVE:
+						return true;
+					case CRunkcwctrl.CND_HASFOCUS:
+						return this.rh.rhApp.bActivated;
+					case CRunkcwctrl.CND_ISATTACHEDTODESKTOP:
+						return false;
+				}
+				return false;
+			},
+
+			// Expressions
+			// --------------------------------------------
+			getScreenWidth:        function ()
+			{
+				return screen.width;
+			},
+			getScreenHeight:       function ()
+			{
+				return screen.height;
+			},
+			getWindowWidth:        function ()
+			{
+				var winW = 630;
+				if (document.body && document.body.offsetWidth)
+				{
+					winW = document.body.offsetWidth;
+				}
+				if (document.compatMode == 'CSS1Compat' &&
+					document.documentElement &&
+					document.documentElement.offsetWidth)
+				{
+					winW = document.documentElement.offsetWidth;
+				}
+				if (window.innerWidth && window.innerHeight)
+				{
+					winW = window.innerWidth;
+				}
+				return winW;
+			},
+			getWindowHeight:       function ()
+			{
+				return winH;
+			},
+			expression:            function (num)
+			{
+				switch (num)
+				{
+					case CRunkcwctrl.EXP_GETXPOSITION:
+						return (0);
+					case CRunkcwctrl.EXP_GETYPOSITION:
+						return (0);
+					case CRunkcwctrl.EXP_GETXSIZE:
+						return window.outerWidth;
+					case CRunkcwctrl.EXP_GETYSIZE:
+						return window.outerHeight;
+					case CRunkcwctrl.EXP_GETSCREENXSIZE:
+						return this.getScreenWidth();
+					case CRunkcwctrl.EXP_GETSCREENYSIZE:
+						return this.getScreenHeight();
+					case CRunkcwctrl.EXP_GETSCREENDEPTH:
+						return window.colorDepth;
+					case CRunkcwctrl.EXP_GETCLIENTXSIZE:
+						return window.innerWidth;
+					case CRunkcwctrl.EXP_GETCLIENTYSIZE:
+						return window.innerHeight;
+					case CRunkcwctrl.EXP_GETTITLE:
+						return document.title;
+					case CRunkcwctrl.EXP_GETBACKCOLOR:
+						return (this.rh.rhApp.gaBorderColour);
+					case CRunkcwctrl.EXP_GETXFRAME:
+						return (0);
+					case CRunkcwctrl.EXP_GETYFRAME:
+						return (0);
+					case CRunkcwctrl.EXP_GETWFRAME:
+						return this.rh.rhApp.gaCxWin;
+					case CRunkcwctrl.EXP_GETHFRAME:
+						return this.rh.rhApp.gaCyWin;
+				}
+				return 0;
+			}
+		});
 
 
 	Runtime(__can, __path); 
